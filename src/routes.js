fca8c73192cd47d5c8bcaf575812ccb58d73fa77
osbert <osbert1996@gmail.com>
2021-09-30 23:47:03 +0700

    Setup route and implement dummy pages

---
+++ src/routes.js
@@ -0,0 +1,75 @@
+import React, { lazy, Suspense, Fragment } from 'react';
+import { Redirect, Switch, Route } from 'react-router-dom';
+import WithSubnavigation from 'components/layouts';
+
+const routesConfig = [
+  {
+    path: '/',
+    exact: true,
+    component: () => <Redirect to="/leaderboard" />,
+  },
+  {
+    path: '/',
+    layout: WithSubnavigation,
+    routes: [
+      {
+        path: '/leaderboard',
+        exact: true,
+        component: lazy(() => import('views/Leaderboard')),
+      },
+      {
+        path: '/explorer',
+        exact: true,
+        component: lazy(() => import('views/Leaderboard')),
+      },
+    ],
+  },
+  {
+    path: '*',
+    component: () => <Redirect to="/errors/error-404" />,
+  },
+];
+
+function renderRoutes(routes, user) {
+  return routes ? (
+    <Suspense
+      fallback={
+        <div style={{ width: '100%', textAlign: 'center', marginTop: 64 }}>
+          {/* <CircularProgress /> */}asd
+        </div>
+      }
+    >
+      <Switch>
+        {routes.map((route, index) => {
+          const Layout = route.layout || Fragment;
+          const Component = route.component;
+
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              render={props => {
+                return (
+                  <Layout>
+                    {route.routes ? (
+                      renderRoutes(route.routes, user)
+                    ) : (
+                      <Component {...props} />
+                    )}
+                  </Layout>
+                );
+              }}
+            />
+          );
+        })}
+      </Switch>
+    </Suspense>
+  ) : null;
+}
+
+const Routes = () => {
+  return renderRoutes(routesConfig);
+};
+
+export default Routes;
---
+++ src/views/Leaderboard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Leaderboard = () => {
+  return (
+    <div>
+      Et dolor adipisicing Lorem dolore aliquip voluptate mollit laborum sit.
+      Adipisicing sit ea cupidatat id ipsum laborum veniam voluptate cillum.
+      Fugiat mollit esse quis est commodo excepteur amet culpa aute culpa et
+      dolor. Pariatur pariatur amet ullamco aliquip proident veniam aliquip
+      minim aute veniam est Lorem. Ullamco proident dolore consectetur aliquip.
+      Culpa consequat nulla ea incididunt ullamco aliquip Lorem officia aliquip
+      deserunt. Sit voluptate nisi aute consequat ipsum proident. Irure
+      incididunt pariatur sit anim officia nulla ullamco dolore labore cupidatat
+      Lorem culpa sit. Commodo aliqua labore pariatur ex velit dolor amet
+      laborum in ea reprehenderit eu nostrud non. Ullamco duis enim excepteur ex
+      reprehenderit elit do incididunt enim irure sint nulla. Consectetur et id
+      pariatur elit ad sunt culpa laborum.
+    </div>
+  );
+};
+
+export default Leaderboard;
